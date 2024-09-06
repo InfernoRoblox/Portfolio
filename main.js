7ff32f43c340bb21a224cce6eaed188c2f065568
InfernoRoblox <81739206+InfernoRoblox@users.noreply.github.com>
2024-09-06 11:58:58 +0800

    minor change

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ function scrollToElement(elementSelector, instance = 0) {
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4")
 
 link1.addEventListener('click', () => {
     scrollToElement('.header');
@@ -18,9 +19,9 @@ link1.addEventListener('click', () => {
 
 link2.addEventListener('click', () => {
     // Scroll to the second element with "header" class
-    scrollToElement('.header', 1);
+    scrollToElement('.features');
 });
 
 link3.addEventListener('click', () => {
-    scrollToElement('.column');
+    scrollToElement('.pricing');
 });
